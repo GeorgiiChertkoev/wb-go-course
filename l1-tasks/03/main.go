56ae980f53cbf2712ae7a44fbaffc4cf37c4f8ba
GeorgiiChertkoev <geochert13028@gmail.com>
2025-08-20 13:47:15 +0300

    03

--- l1-tasks/03/main.go
+++ l1-tasks/03/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func main() {
+	numberOfWorkers := 50
+	var wg sync.WaitGroup
+	wg.Add(numberOfWorkers)
+	ch := make(chan any, numberOfWorkers) // канал с буффером на кол-во горутин
+
+	for range numberOfWorkers {
+		go func() {
+			defer wg.Done()
+			for {
+				value, ok := <-ch
+				if !ok { // выйдем из горутины когда значения в канале закончатся и он будет закрыт
+					break
+				}
+				fmt.Println(value)
+			}
+		}()
+	}
+
+	for i := range 100 {
+		ch <- i // отправляем данные в канал
+	}
+	close(ch)
+
+	wg.Wait() // ждем когда все горутины закончат
 }
