48e26ec07f53afa1f71baaf03a35f9ec47754f83
GeorgiiChertkoev <geochert13028@gmail.com>
2025-08-20 13:16:10 +0300

    02 done

--- l1-tasks/02/main.go
+++ l1-tasks/02/main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+func solution1(nums []int) {
+	var wg sync.WaitGroup
+	for _, value := range nums {
+		wg.Add(1)
+		go func(num int) {
+			defer wg.Done()
+			fmt.Println(num * num)
+		}(value)
+	}
+	wg.Wait()
+}
+
+func solution2(nums []int) {
+	results := make(chan int)
+	for _, value := range nums {
+		go func(num int) {
+			results <- num * num
+		}(value)
+	}
+	for i := 0; i < len(nums); i++ {
+		fmt.Println(<-results)
+	}
+
+}
+
 func main() {
+	array := []int{2, 4, 6, 8, 10}
+	solution1(array)
+	fmt.Println()
+	solution2(array)
 
 }
