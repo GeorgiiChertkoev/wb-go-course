cfca7d95d63baf71490ac5208f64b5a7b2b3817a
GeorgiiChertkoev <geochert13028@gmail.com>
2025-08-24 17:56:52 +0300

    08

--- l1-tasks/08/main.go
+++ l1-tasks/08/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+const countingBitsFromOne = true
+
+func changeBit(num int64, position int, value bool) int64 {
+	if countingBitsFromOne {
+		position-- // внутри функции я считаю с нуля
+	}
+	if value {
+		num |= 1 << position
+	} else {
+		num &= -1 ^ (1 << position) // у -1 все биты единички в двоичном представлении
+	}
+	return num
+}
+
+func main() {
+	n := int64(5)
+	fmt.Printf("was: %x, got %x", n, changeBit(n, 1, false))
 }
