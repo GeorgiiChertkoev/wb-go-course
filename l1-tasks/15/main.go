4dc9ed46f1e369f7371ce09cd9cd967dc7fdca16
GeorgiiChertkoev <geochert13028@gmail.com>
2025-08-26 18:53:15 +0300

    15

--- l1-tasks/15/main.go
+++ l1-tasks/15/main.go
@@ -1,18 +1,36 @@
 package main
 
-import "fmt"
+var justString string
+
+//	func someFunc() {
+//		v := createHugeString(1 << 10)
+//		justString = v[:100]
+//	}
+
+// в старом решении мы создали слайс на 1024 байта
+// но потом используем только первые 100
+// как результат - утечка памяти т.к. сборщик мусора
+// не может очистить всю строку так как на часть ее
+// ссылается слайс несмотря на то что слайс использует только 100 байт из 1024
+
+func someFunc() {
+	// для решения такой проблемы я создаю новый слайс на 100 байт
+	// и в него перекопирую только первые 100 символов строки
+	// после этого на длинную строку не кто не ссылается и сборщик мусора ее очистит
+	v := createHugeString(1 << 10)
+	temp := make([]byte, 100)
+	copy(temp, v[:100])
+	justString = string(temp)
+}
 
 func main() {
-	words := []string{"cat", "cat", "dog", "cat", "tree"}
-	m := make(map[string]bool)
-	for _, v := range words {
-		m[v] = true
-	}
+	someFunc()
+}
 
-	uniqueWords := make([]string, 0, len(m))
-	for k, _ := range m {
-		uniqueWords = append(uniqueWords, k)
+func createHugeString(length int) string {
+	s := make([]byte, length)
+	for i := 0; i < length; i++ {
+		s[i] = 'a'
 	}
-
-	fmt.Printf("Uniques: %v\n", uniqueWords)
+	return string(s)
 }
