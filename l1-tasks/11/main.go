ff5430479670701b91c326fc95717d5a848cb8a8
GeorgiiChertkoev <geochert13028@gmail.com>
2025-08-26 13:35:30 +0300

    11

--- l1-tasks/11/main.go
+++ l1-tasks/11/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+func Intersect[T comparable](a, b []T) []T {
+	if len(a) > len(b) { // хотим добавить в мапу меньше ключей
+		a, b = b, a
+	}
+
+	m := make(map[T]bool)
+	for _, v := range a {
+		m[v] = true
+	}
+
+	res := make([]T, 0, len(m))
+
+	for _, v := range b {
+		if m[v] {
+			res = append(res, v)
+		}
+	}
+	return res
+}
+
+func main() {
+	a := []int{1, 2, 3, 4}
+	b := []int{5, 2, 4}
+
+	fmt.Printf("%v\n", Intersect(a, b))
 }
