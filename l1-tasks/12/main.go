ddb3a28f7e6f40cc8eec7d21a33ca851efd3bfdd
GeorgiiChertkoev <geochert13028@gmail.com>
2025-08-26 13:42:21 +0300

    12

--- l1-tasks/12/main.go
+++ l1-tasks/12/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	words := []string{"cat", "cat", "dog", "cat", "tree"}
+	m := make(map[string]bool)
+	for _, v := range words {
+		m[v] = true
+	}
+
+	uniqueWords := make([]string, 0, len(m))
+	for k, _ := range m {
+		uniqueWords = append(uniqueWords, k)
+	}
+
+	fmt.Printf("Uniques: %v\n", uniqueWords)
 }
