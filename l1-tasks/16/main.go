95a3f8720fc241216c2dff7cdcb24b6a0a8be24f
GeorgiiChertkoev <geochert13028@gmail.com>
2025-08-27 23:49:51 +0300

    16

--- l1-tasks/16/main.go
+++ l1-tasks/16/main.go
@@ -2,17 +2,48 @@ package main
 
 import "fmt"
 
+func partition(nums []int, l int, r int) int {
+	pivot := nums[(l+r)/2]
+	for l < r {
+		for nums[l] < pivot {
+			l++
+		}
+		for nums[r] > pivot {
+			r--
+		}
+		if l >= r {
+			break
+		}
+		l++
+		r--
+		nums[l], nums[r] = nums[r], nums[l]
+	}
+	return r
+}
+
+func quickSortInplaceImpl(nums []int, l int, r int) {
+	if l < r {
+		pviotPos := partition(nums, l, r)
+		quickSortInplaceImpl(nums, l, pviotPos)
+		quickSortInplaceImpl(nums, pviotPos+1, r)
+	}
+}
+
+func QuickSortInplace(nums []int) {
+	quickSortInplaceImpl(nums, 0, len(nums)-1)
+}
+
+func QuickSort(nums []int) []int {
+	sorted := make([]int, len(nums))
+	copy(sorted, nums)
+	QuickSortInplace(sorted)
+	return sorted
+}
+
 func main() {
-	words := []string{"cat", "cat", "dog", "cat", "tree"}
-	m := make(map[string]bool)
-	for _, v := range words {
-		m[v] = true
-	}
-
-	uniqueWords := make([]string, 0, len(m))
-	for k, _ := range m {
-		uniqueWords = append(uniqueWords, k)
-	}
-
-	fmt.Printf("Uniques: %v\n", uniqueWords)
+	// nums := []int{14, 5, 5, 5, 5, 1, 45, 7, 2, 4}
+	nums := []int{5, 5, 14, 45, 7, 5, 5}
+	sorted := QuickSort(nums)
+	fmt.Printf("was: %v\n", nums)
+	fmt.Printf("got: %v\n", sorted)
 }
