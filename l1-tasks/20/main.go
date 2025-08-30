f32d357195fb5671fbdaa58eedc0503f5d78d96e
GeorgiiChertkoev <geochert13028@gmail.com>
2025-08-30 23:22:21 +0300

    20

--- l1-tasks/20/main.go
+++ l1-tasks/20/main.go
@@ -1,18 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+	"strings"
+	"unsafe"
+)
+
+func SimplestSolution(s string) string {
+	words := strings.Split(s, " ")
+	slices.Reverse(words)
+	return strings.Join(words, " ")
+}
+
+func WithOneCopy(s string) string {
+	reversed := make([]byte, len(s))
+
+	l := 0 // начало нынешнего слова
+	for i := 0; i < len(s); i++ {
+		if s[i] == ' ' {
+			for j := l; j < i; j++ {
+				reversed[len(s)-i+j-l] = s[j]
+			}
+			if l != 0 { // добавляем пробел для не первого слова
+				reversed[len(s)-l] = ' '
+			}
+			l = i + 1
+		}
+	}
+	// добавляем последнее слово в начало
+	for j := 0; j < len(s)-l; j++ {
+		reversed[j] = s[l+j]
+	}
+	reversed[len(s)-l] = ' '
+
+	return unsafe.String(&reversed[0], len(reversed)) // избегаем ненужного копирования напирямую отдавая наш массив в строку
+}
 
 func main() {
-	words := []string{"cat", "cat", "dog", "cat", "tree"}
-	m := make(map[string]bool)
-	for _, v := range words {
-		m[v] = true
-	}
+	sentance := string("прив как дела й окак")
 
-	uniqueWords := make([]string, 0, len(m))
-	for k, _ := range m {
-		uniqueWords = append(uniqueWords, k)
-	}
-
-	fmt.Printf("Uniques: %v\n", uniqueWords)
+	fmt.Printf("was: \"%s\" \n", sentance)
+	fmt.Printf("got: \"%s\" \n", SimplestSolution(sentance))
+	fmt.Printf("got: \"%s\" \n", WithOneCopy(sentance))
 }
