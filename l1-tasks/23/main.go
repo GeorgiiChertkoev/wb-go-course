8a806ebadc001399cf18c71bc388338a9be72cb6
GeorgiiChertkoev <geochert13028@gmail.com>
2025-09-01 22:11:42 +0300

    23

--- l1-tasks/23/main.go
+++ l1-tasks/23/main.go
@@ -2,17 +2,22 @@ package main
 
 import "fmt"
 
+func DeleteElem[T any](slice []T, i int) []T {
+	// 3 разных способа
+
+	// slice = slices.Delete(slice, i, i + 1)
+
+	// slice = append(slice[:i], slice[i+1:]...)
+
+	copy(slice[i:], slice[i+1:]) // перекомпируем элементы со сдвигом на один
+	slice = slice[:len(slice)-1] // сокращаем длину
+
+	return slice
+}
+
 func main() {
-	words := []string{"cat", "cat", "dog", "cat", "tree"}
-	m := make(map[string]bool)
-	for _, v := range words {
-		m[v] = true
-	}
-
-	uniqueWords := make([]string, 0, len(m))
-	for k, _ := range m {
-		uniqueWords = append(uniqueWords, k)
-	}
-
-	fmt.Printf("Uniques: %v\n", uniqueWords)
+	nums := []int{1, 2, 3, 4, 5, 6, 7}
+	fmt.Printf("%v len:%v cap:%v\n", nums, len(nums), cap(nums))
+	nums = DeleteElem(nums, 2)
+	fmt.Printf("%v len:%v cap:%v\n", nums, len(nums), cap(nums))
 }
