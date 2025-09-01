7aea32f0af6797ff0fec075d9bea6a269c12706a
GeorgiiChertkoev <geochert13028@gmail.com>
2025-09-01 22:25:28 +0300

    24

--- l1-tasks/24/main.go
+++ l1-tasks/24/main.go
@@ -1,18 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	x float64
+	y float64
+}
+
+func NewPoint(x, y float64) Point {
+	return Point{
+		x: x,
+		y: y,
+	}
+}
+
+func (p *Point) Distance(other Point) float64 {
+	return math.Sqrt(math.Pow((p.x-other.x), 2) + math.Pow((p.y-other.y), 2))
+}
 
 func main() {
-	words := []string{"cat", "cat", "dog", "cat", "tree"}
-	m := make(map[string]bool)
-	for _, v := range words {
-		m[v] = true
-	}
+	p1 := NewPoint(1, 1)
+	p2 := NewPoint(2, 2)
 
-	uniqueWords := make([]string, 0, len(m))
-	for k, _ := range m {
-		uniqueWords = append(uniqueWords, k)
-	}
+	fmt.Println(p1, p2, p1.Distance(p2))
 
-	fmt.Printf("Uniques: %v\n", uniqueWords)
 }
