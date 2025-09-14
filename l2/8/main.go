d86fe5eaa6b659587a6eba14095653634c00e550
GeorgiiChertkoev <geochert13028@gmail.com>
2025-09-14 14:31:01 +0300

    2.8

--- l2/8/main.go
+++ l2/8/main.go
@@ -8,12 +8,21 @@ import (
 	"github.com/beevik/ntp"
 )
 
-func main() {
-	ntpTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Ошибка получения времени: %s\n", err)
-		os.Exit(1)
-	}
+const StandartServerAddress = "0.beevik-ntp.pool.ntp.org"
 
-	fmt.Printf("Нынешнее время: %s", ntpTime.Format(time.RFC3339))
+func GetNtpTime(address string) (time.Time, error) {
+	return ntp.Time(address)
+}
+
+func main() {
+	for {
+		ntpTime, err := GetNtpTime(StandartServerAddress)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Ошибка получения времени: %s\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("Нынешнее время: %s\n", ntpTime.Format(time.RFC3339))
+		<-time.After(1 * time.Second)
+	}
 }
